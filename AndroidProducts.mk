466f11e6f3c84589296eb8caf82f72bee2552386
Adam Lawson <adamjlawson22@gmail.com>
2021-05-01 16:09:42 +0100

    RMX1971: Bringup LineageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_RMX1971.mk
+    $(LOCAL_DIR)/lineage_RMX1971.mk
--- lineage_RMX1971.mk
+++ lineage_RMX1971.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common ancientOS stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -16,15 +16,10 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
-ANCIENT_GAPPS := true
-
-# Official-ify
-ANCIENT_OFFICIAL=true
-TARGET_USES_BLUR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := ancient_RMX1971
+PRODUCT_NAME := lineage_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
