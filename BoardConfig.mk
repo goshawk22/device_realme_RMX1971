174a306cf4cf34fd315d0585eb4dce8d639c7cb0
Adam Lawson <adamjlawson22@gmail.com>
2021-05-25 19:58:35 +0100

    RMX1971: Compile with Proton Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,13 @@ TARGET_OTA_ASSERT_DEVICE := RMX1971,RMX1971CN
 # Kernel
 TARGET_KERNEL_CONFIG := KharaMe_defconfig
 
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS += AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32 := arm-linux-gnueabi-
+
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Verified Boot
