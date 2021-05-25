fca794860087f9d9bd45920bf2a22c4b2cfed15a
Adam Lawson <adamjlawson22@gmail.com>
2021-05-25 19:58:35 +0100

    RMX1971: Compile with Proton Clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,13 @@ TARGET_OTA_ASSERT_DEVICE := RMX1971,RMX1971CN
 TARGET_KERNEL_CONFIG := sdm670-perf_defconfig
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS += AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32 := arm-linux-gnueabi-
+
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Verified Boot
