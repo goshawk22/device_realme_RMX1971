aae1d4ece796cdb1b19c5422dcdffec4875fa84e
Adam Lawson <adamjlawson22@gmail.com>
2021-08-11 11:00:36 +0100

    RMX1971: Octavi Official!

--- octavi_RMX1971.mk
+++ octavi_RMX1971.mk
@@ -13,6 +13,12 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
+# Octavi Official Stuff
+TARGET_SCREEN_DENSITY := 420
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_DEVICE_MAINTAINER := goshawk22
+WITH_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
 PRODUCT_NAME := octavi_RMX1971
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,27 @@
-rm -rf hardware/qcom/audio/adsprpcd
-rm -rf hardware/qcom-caf/sdm845/audio
-rm -rf hardware/qcom-caf/sdm845/media
-rm -rf hardware/qcom-caf/sdm845/display
-
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
-git clone https://github.com/goshawk22/hardware_qcom_display -b octavi-caf-sdm845 hardware/qcom-caf/sdm845/display
-
-git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
-
-git clone github.com/goshawk22/android_device_realme_sdm710-common -b octavi-11 device/realme/sdm710-common
-git clone https://github.com/goshawk22/kernel_realme_RMX1971 -b 11 kernel/realme/RMX1971
-git clone https://gitlab.com/MadhavSaladi/vendor_realme_sdm710-common vendor/realme/sdm710-common
-git clone https://gitlab.com/MadhavSaladi/vendor_realme_RMX1971 vendor/realme/RMX1971
+echo "Clone Everything? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+	echo "Cloning!"
+        clone
+else
+        echo "Not cloning!"
+fi
 
 
+function clone {
+	rm -rf hardware/qcom/audio/adsprpcd
+	rm -rf hardware/qcom-caf/sdm845/audio
+	rm -rf hardware/qcom-caf/sdm845/media
+	rm -rf hardware/qcom-caf/sdm845/display
+
+	git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
+	git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-sdm845 hardware/qcom-caf/sdm845/media
+	git clone https://github.com/goshawk22/hardware_qcom_display -b octavi-caf-sdm845 hardware/qcom-caf/sdm845/display
+
+	git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+
+	git clone github.com/goshawk22/android_device_realme_sdm710-common -b octavi-11 device/realme/sdm710-common
+	git clone https://github.com/goshawk22/kernel_realme_RMX1971 -b 11 kernel/realme/RMX1971
+	git clone https://gitlab.com/MadhavSaladi/vendor_realme_sdm710-common vendor/realme/sdm710-common
+	git clone https://gitlab.com/MadhavSaladi/vendor_realme_RMX1971 vendor/realme/RMX1971
+}
