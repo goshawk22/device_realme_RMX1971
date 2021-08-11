32ca2224e761158542b49f657a63a53b6df67ae6
Adam Lawson <adamjlawson22@gmail.com>
2021-08-11 11:33:02 +0100

    RMX1971: Improve vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+if ! [ -d prebuilts/clang/host/linux-x86/clang-proton ]; then git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton; fi
 
-git clone github.com/goshawk22/android_device_realme_sdm710-common -b e device/realme/sdm710-common
-git clone https://github.com/goshawk22/kernel_realme_RMX1971 -b 11 kernel/realme/RMX1971
-git clone https://gitlab.com/MadhavSaladi/vendor_realme_sdm710-common vendor/realme/sdm710-common
-git clone https://gitlab.com/MadhavSaladi/vendor_realme_RMX1971 vendor/realme/RMX1971
+if ! [ -d device/realme/sdm710-common ]; then git clone https://github.com/goshawk22/android_device_realme_sdm710-common -b e device/realme/sdm710-common; fi
+if ! [ -d kernel/realme/RMX1971 ]; then git clone https://github.com/goshawk22/kernel_realme_RMX1971 -b 11 kernel/realme/RMX1971; fi
+if ! [ -d vendor/realme/sdm710-common ]; then git clone https://gitlab.com/MadhavSaladi/vendor_realme_sdm710-common vendor/realme/sdm710-common; fi
+if ! [ -d vendor/realme/RMX1971 ]; then git clone https://gitlab.com/MadhavSaladi/vendor_realme_RMX1971 vendor/realme/RMX1971; fi
