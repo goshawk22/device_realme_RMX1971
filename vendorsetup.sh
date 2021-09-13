ab7134053843202f0c7718e885b381a51f768294
Adam Lawson <adamjlawson22@gmail.com>
2021-09-13 17:12:26 +0100

    RMX1971: Fix clone of common tree in vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,7 +20,7 @@ function clone {
 
 	git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
 
-	git clone github.com/goshawk22/android_device_realme_sdm710-common -b octavi-11 device/realme/sdm710-common
+	git clone https://github.com/goshawk22/android_device_realme_sdm710-common -b octavi-11 device/realme/sdm710-common
 	git clone https://github.com/goshawk22/kernel_realme_RMX1971 -b 11 kernel/realme/RMX1971
 	git clone https://gitlab.com/MadhavSaladi/vendor_realme_sdm710-common vendor/realme/sdm710-common
 	git clone https://gitlab.com/MadhavSaladi/vendor_realme_RMX1971 vendor/realme/RMX1971
