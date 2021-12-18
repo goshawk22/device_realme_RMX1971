3bff38b5bfe732e6cdb4b3867a07f707d1949011
Adam Lawson <adamjlawson22@gmail.com>
2021-12-18 10:52:27 +0000

    RMX1971: Bringup OctaviOS

--- octavi_RMX1971.mk
+++ octavi_RMX1971.mk
@@ -4,17 +4,21 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common AEX stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common OctaviOS stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# OctaviOS Official
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_DEVICE_MAINTAINER := goshawk22
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := aosp_RMX1971
+PRODUCT_NAME := octavi_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
